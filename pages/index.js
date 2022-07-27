80a8581db02f223183b5c8cb5e0ab26d4b676f81
ekbrier <eleanor_brierley@hotmail.com>
2022-07-27 22:31:39 +0100

    removed unnecessary pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link';
-import { useState } from 'react';
+import styles from '../styles/index.module.css'
+import { useState, useEffect } from 'react';
+import Card from '../components/Card.js';
 
-export default function Home() { 
+
+export default function Index() {
+  const [Messages, setMessages] = useState([]);
+
+
+  async function getdata() {
+    const response = await fetch("https://dev.api.messageboard.aws.ellio.me/api/messages")
+    const data = await response.json()
+    const example_response = await fetch("https://dev.api.messageboard.aws.ellio.me/api/example")
+    const example_data = await example_response.json()
+
+    setMessages(data.data)
+
+  }
+
+  useEffect(function () {
+    getdata()
+
+  }, []);
   return (
-    <div className={styles.container}>
-  </div>
+    <main className={styles.page}>
+      <span className={styles.container}>
+        <div className={styles.input_box}>
+          <form>
+            <textarea className={styles.input_message} placeholder={"Type your message..."} id="text" maxLength="280"></textarea>
+            <div>
+              <button className={styles.send_button}>Send</button>
+            </div>
+          </form>
+        </div>
+
+        {Messages.map((card, index) => <Card message={card.attributes.body} username={card.attributes.username} key={index}></Card>
+        )}
+      </span>
+    </main>
+
   )
 }
